71301e09d756aba33878417a71e2f52fd295d265
kcho0302 <kwdc0302@gmail.com>
2024-02-25 23:03:24 +0900

    feature: starts of oauth

---
+++ kdev/src/main/java/com/kwDevStudy/kdev/KdevApplication.java
@@ -0,0 +1,13 @@
+package com.kwDevStudy.kdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdevApplication.class, args);
+	}
+
+}
